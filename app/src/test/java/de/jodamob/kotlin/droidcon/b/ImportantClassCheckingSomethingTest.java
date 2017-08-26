90880d94302e93c72a45e17378ba2ce5bcf64caf
Danny Preussler <danny.preussler@vimn.com>
2017-08-26 12:15:20 +0200

    moved more files

--- app/src/test/java/de/jodamob/kotlin/droidcon/b/ImportantClassCheckingSomethingTest.java
+++ app/src/test/java/de/jodamob/kotlin/droidcon/b/ImportantClassCheckingSomethingTest.java
@@ -1,13 +1,14 @@
-package de.jodamob.kotlin.droidcon;
+package de.jodamob.kotlin.droidcon.b;
 
 import org.junit.Test;
-
 import de.jodamob.kotlin.droidcon.a.ImportantClassDoingSomething;
+
+
 public class ImportantClassCheckingSomethingTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void should_throw_if_empy() {
-        ImportantClassDoingSomething tested = new ImportantClassDoingSomething();
-        tested.makeItHappen("");
+        ImportantClassCheckingSomething tested = new ImportantClassCheckingSomething();
+        tested.dontletItHappen("");
     }
 }
