baecb584fc60f6293e49deebfb0b74a9e6ea48ea
Danny Preussler <danny.preussler@vimn.com>
2017-08-26 12:09:02 +0200

    fixed test

--- app/src/main/java/de/jodamob/kotlin/droidcon/a/ImportantClassDoingSomething.java
+++ app/src/main/java/de/jodamob/kotlin/droidcon/a/ImportantClassDoingSomething.java
@@ -3,5 +3,7 @@ package de.jodamob.kotlin.droidcon.a;
 public class ImportantClassDoingSomething {
 
     public void makeItHappen(String argument) {
+        throw new IllegalArgumentException();
+
     }
 }
