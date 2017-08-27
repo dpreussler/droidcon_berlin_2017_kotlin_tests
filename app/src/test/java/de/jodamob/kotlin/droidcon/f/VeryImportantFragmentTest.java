5dbc92767c2bdd8757993ec09f618f8cc5c339da
Danny Preussler <danny.preussler@vimn.com>
2017-08-27 18:24:03 +0200

    setup

--- app/src/test/java/de/jodamob/kotlin/droidcon/f/VeryImportantFragmentTest.java
+++ app/src/test/java/de/jodamob/kotlin/droidcon/f/VeryImportantFragmentTest.java
@@ -2,24 +2,32 @@ package de.jodamob.kotlin.droidcon.f;
 
 import android.os.Bundle;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class VeryImportantFragmentTest {
 
-    Bundle bundle = mock(Bundle.class);
+    @Mock
+    Bundle bundle;
     VeryImportantFragment tested = new VeryImportantFragment();
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        when(bundle.getString(anyString())).thenReturn("this is a test");
+    }
 
     @Test
     public void readFromBundle() {
-        when(bundle.getString(anyString())).thenReturn("this is a test");
         tested.onCreate(bundle);
         verify(bundle).getString("secretKey1");
         assertEquals("this is a test", tested.getImportantState());
