2ddfc01af2212f6d1b88f41f863b4a594b7e6d9c
Danny Preussler <danny.preussler@vimn.com>
2017-09-05 08:16:40 +0200

    added both versions for crash

--- app/src/test/java/de/jodamob/kotlin/droidcon/b/ImportantClassCheckingSomethingTest.kt
+++ app/src/test/java/de/jodamob/kotlin/droidcon/b/ImportantClassCheckingSomethingTest.kt
@@ -3,12 +3,22 @@ package de.jodamob.kotlin.droidcon.b
 import org.amshove.kluent.`with message`
 import org.amshove.kluent.shouldThrow
 import org.junit.Test
+import kotlin.test.assertFailsWith
 
 
 class ImportantClassCheckingSomethingTest {
 
     @Test
-    fun should_throw_if_empty() {
+    fun `should throw if empty normal version`() {
+        val tested = ImportantClassCheckingSomething()
+        
+        assertFailsWith(IllegalArgumentException::class) {
+            tested.dontletItHappen("")
+        }
+    }
+
+    @Test
+    fun `should throw if empty kluent version`() {
         val tested = ImportantClassCheckingSomething()
         val function = {tested.dontletItHappen("")}
 
